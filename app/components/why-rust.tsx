2bbbb3e8b756f05371a1ead0fbfd8c65d482cc10
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-05-29 00:42:33 +0200

    Add initial why-rust

---
+++ app/components/why-rust.tsx
@@ -0,0 +1,41 @@
+import styles from "./why-rust.module.css";
+
+const WhyRust = () => (
+  <div className={styles.background}>
+    <section className={styles.whyRust}>
+      <h1>Why Rust?</h1>
+
+      <div className={styles.container}>
+        <article>
+          <h1>Performance</h1>
+          <p>
+            Rust is blazingly fast and memory-efficient: with no runtime or
+            garbage collector, it can power performance-critical services, run
+            on embedded devices, and esily integrate with other languages.
+          </p>
+        </article>
+
+        <article>
+          <h1>Reliability</h1>
+          <p>
+            Rust&apos;s rich type system and ownership model guarantee
+            memory-safety and thread-safety — enabling you to elminate many
+            classes of bugs at compile-time.
+          </p>
+        </article>
+
+        <article>
+          <h1>Productivity</h1>
+          <p>
+            Rust has great documentation, a friendly compiler with useful error
+            messages, and top-noth tooling - an integrated package manager and
+            build tool, smart multi-editor support with atuto-completion and
+            type inspections, an auto-formatter, and more.
+          </p>
+        </article>
+      </div>
+    </section>
+  </div>
+);
+
+export default WhyRust;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "./components/hero";
+import WhyRust from "./components/why-rust";
 
 import styles from "./page.module.css";
 
 const Page = () => (
   <main className={styles.main}>
     <Hero />
+    <WhyRust />
   </main>
 );
 
