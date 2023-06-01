1a9e84825d4e1eeda3c966a358c42145226ebe15
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-06-01 10:00:17 +0200

    Adjust font styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Hero from "./components/hero";
 import WhyRust from "./components/why-rust";
 import BuildItInRust from "./components/build-it-in-rust";
 import RustInProduction from "./components/rust-in-production";
+import GetInvolved from "./components/get-involved";
 
 import styles from "./page.module.css";
-import GetInvolved from "./components/get-involved";
 
 const Page = () => (
   <main className={styles.main}>
